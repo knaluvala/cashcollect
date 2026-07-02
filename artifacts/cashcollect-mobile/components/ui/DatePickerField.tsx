3b963d0910aa07bee419afd7eac28ce755bbd951
knaluvala <knaluvala@galadaribrothers.com>
2026-07-02 18:23:35 +0400

    Add reusable date picker field component scaffold

---
+++ artifacts/cashcollect-mobile/components/ui/DatePickerField.tsx
@@ -0,0 +1,9 @@
+export type DatePickerFieldProps = {
+  value: string;
+  onBPress: () => void;
+};
+
+export function DatePickerField(props: DatePickerFieldProps) {
+  void props;
+  return null;
+}
